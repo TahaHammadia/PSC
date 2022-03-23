bdb561d40e6e2b02ac5203364f74a0dded01b117
TahaHammadia <75761263+TahaHammadia@users.noreply.github.com>
2022-03-23 14:18:45 +0100

    up

--- realTinder.py
+++ realTinder.py
@@ -4,8 +4,8 @@ from PIL import Image
 
 print("Écrivez 1 si vous pensez qu'il s'agit d'un cas de charge, 0 sinon. Si vous voulez finir, écriver -1.")
 
-folder1 = "C:/Users/hp 650 G3/Documents/GitHub/PSC/data/Pics/"
-folder2 = "C:/Users/hp 650 G3/Documents/GitHub/PSC/data/Pics [11, 6, 2, 1, 1] [210, 60, 1, 4, 4, 3, 28, 3, 14, 3, 15]/"
+folder2 = "C:/Users/hp 650 G3/Documents/GitHub/PSC/data/Pics/"
+folder1 = "C:/Users/hp 650 G3/Documents/GitHub/PSC/data/Pics [11, 6, 2, 1, 1] [210, 60, 1, 4, 4, 3, 28, 3, 14, 3, 15]/"
 
 with open(folder1 + "etat.txt", 'r') as f:
     cpt = int(f.readline())
